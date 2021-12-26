ab670c5d9886dabfa8383c5f8f728a0e4aff6beb
Kayahan Şanlı <kayahansan24@gmail.com>
2021-12-26 21:17:26 +0300

    GüncelBlog

---
+++ WebProgramlamaProjesi/Migrations/20211226175439_blogson.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebProgramlamaProjesi.Migrations
+{
+    public partial class blogson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
